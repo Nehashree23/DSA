e3f1c91c130fc827ebe1710eac832fd6ab9a0aec
Nehashree23 <165762589+Nehashree23@users.noreply.github.com>
2025-08-22 20:51:33 +0530

    Create reverse_a_string.java

---
+++ two pointer/reverse_a_string.java
@@ -0,0 +1,19 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        System.out.print("[");
+        for (int i = 0; i < s.length; i++) {
+            System.out.print(s[i]);
+            if (i < s.length - 1) System.out.print(", ");
+        }
+        System.out.print("]");
+    }
+}
